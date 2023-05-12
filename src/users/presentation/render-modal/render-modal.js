5077433a2a4bc32edcd03dae114e99b2ab37262c
DevAlejandroAF <alejandroavi95@hotmail.com>
2023-05-12 13:56:10 -0500

    Final curso

--- src/users/presentation/render-modal/render-modal.js
+++ src/users/presentation/render-modal/render-modal.js
@@ -1,11 +1,21 @@
 import modalHtml from './render-modal.html?raw';
+import { User } from '../../models/user';
 import './render-modal.css';
+import { getUserById } from '../../use-cases/get-user-by-id';
 
-let modal, form;
+let modal, form, loadedUser = {};
 
-//TODO: Cargar usuario por ID
-const showModal = () => {
+/**
+ * 
+ * @param {String|Number} id 
+ * @returns 
+ */
+const showModal = async( id ) => {
   modal?.classList.remove('hide-modal');
+  loadedUser = {};
+  if ( !id ) return;
+  const user = await getUserById( id );
+  setFormValues(user);
 }
 
 const hideModal = () => {
@@ -15,9 +25,22 @@ const hideModal = () => {
 
 /**
  * 
- * @param {HTMLDivElement} element 
+ * @param {User} user 
  */
-const renderModal = ( element ) => {
+const setFormValues = ( user ) => {
+  form.querySelector('[name="firstName"]').value = user.firstName;
+  form.querySelector('[name="lastName"]').value = user.lastName;
+  form.querySelector('[name="balance"]').value = user.balance;
+  form.querySelector('[name="isActive"]').value = user.isActive;
+  loadedUser = user;
+}
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ * @param {(userLike) => Promise<void>} callback
+ */
+const renderModal = ( element, callback ) => {
   if ( modal ) return;
   modal = document.createElement('div');
   modal.innerHTML = modalHtml;
@@ -28,10 +51,10 @@ const renderModal = ( element ) => {
       hideModal();
     }
   });
-  form.addEventListener('submit', ( event ) => {
+  form.addEventListener('submit', async( event ) => {
     event.preventDefault();
     const formData = new FormData( form );
-    const userLike = {};
+    const userLike = { ...loadedUser };
     for (const [key, value] of formData ) {
       if (key === 'balance') {
         userLike[key] = +value; // +value: forma corta de parsear string a número
@@ -43,7 +66,7 @@ const renderModal = ( element ) => {
       }
       userLike[key] = value;
     }
-    //TODO: guardar usuario
+    await callback( userLike );
     hideModal();
   })
   element.append( modal );
